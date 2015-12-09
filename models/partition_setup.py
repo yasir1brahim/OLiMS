5faf8db81cb938ed999eb897d7127f9203055851
yasir1brahim <yasir.ibrahim@gmail.com>
2015-12-09 17:01:08 +0500

    Re-factored the relational model from 'Sample Point' to 'Sample Type'.

--- models/partition_setup.py
+++ models/partition_setup.py
@@ -2,7 +2,7 @@
 from openerp import fields, models
 from base_olims_model import BaseOLiMSModel
 
-schema = (fields.Many2one(string='Sample point', comodel_name='olims.sample_point'),
+schema = (fields.Many2one(string='Sample Type', comodel_name='olims.sample_type'),
           fields.Boolean(string='Separate Container'),
           fields.Many2many(string='Preservation', comodel_name='olims.preservation'),
           fields.Many2many(string='Container', comodel_name='olims.container'),
