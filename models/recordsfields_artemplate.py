aaddfcf59652607e97eff4e3b693f74387d7adfa
yasir1brahim <yasir.ibrahim@gmail.com>
2015-12-10 11:55:21 +0500

    Re-factored the AR Template view to have inline form for the field 'Analyses'.

--- models/recordsfields_artemplate.py
+++ models/recordsfields_artemplate.py
@@ -1,16 +1,23 @@
-from openerp import fields, models
+from openerp import fields, models, api
 from base_olims_model import BaseOLiMSModel
 
-schema = (fields.Many2many(string='Services Analysis',
+schema = (fields.Many2one(string='Services',
                    comodel_name='olims.analysis_service',
                    relation='recordfield_service'),
-          fields.Boolean(string='Hidden'),
-          fields.Float(string='Price', default=0.00),
+          fields.Boolean(string='Hidden',readonly=False),
+          fields.Float(string='Price', default=0.00,readonly=True),
         fields.Many2one(string='Partition',
                   comodel_name='olims.partition_ar_template'),
           )
 
 class RecodrdsFieldARTemplate(models.Model, BaseOLiMSModel): 
     _name='olims.records_field_artemplates'
-    
+
+    @api.onchange('Services')
+    def _onchange_postal(self):
+        # set auto-changing field
+        if self.Services:
+            self.Hidden = self.Services.Hidden
+            self.Price = self.Services.Price
+
 RecodrdsFieldARTemplate.initialze(schema)
