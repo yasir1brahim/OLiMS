8ee6aa056e91ba3be298bdc5f1e654907750eb66
yasir1brahim <yasir.ibrahim@gmail.com>
2015-12-16 17:17:04 +0500

    Added the fields '< Min', '> Max' and 'Range Remarks' for the view of specification inline form in Analysis Specification

--- models/specification.py
+++ models/specification.py
@@ -1,13 +1,17 @@
 from openerp import fields, models
 from base_olims_model import BaseOLiMSModel
-
+from fields.text_field import TextField
+from fields.fixed_point_field import FixedPointField
 schema = (fields.Many2one(string='Service',
                 comodel_name='olims.analysis_service',
 #                 relation='analysis_service_specification'
     ),
-    fields.Integer('Min'),
-    fields.Integer('Max'),
-    fields.Integer('Permitted Error'),
+    FixedPointField('Min'),
+    FixedPointField('Max'),
+    FixedPointField('< Min'),
+    FixedPointField('> Max'),
+    FixedPointField('Permitted Error'),
+    TextField('Range remarks:'),
 )
 
 class Specifications(models.Model, BaseOLiMSModel):
