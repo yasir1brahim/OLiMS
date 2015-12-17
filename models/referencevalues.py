9edf9ef48d6f871c5527ea7a8153903f1d41c432
yasir1brahim <yasir.ibrahim@gmail.com>
2015-12-17 15:39:09 +0500

    Re-factored the view of Reference Sample. Added the 'Category' field in inline form of 'Expected Values' to add the functionality to first select the 'Category' and then select the dependent 'Analysis Service' of that category.

--- models/referencevalues.py
+++ models/referencevalues.py
@@ -4,14 +4,17 @@ import logging
 _logger = logging.getLogger(__name__)
 
 schema = (fields.Many2one(string="Service",
-                          comodel_name='olims.analysis_service'),
+                          comodel_name='olims.analysis_service',
+                          domain="[('category', '=', Category)]"),
     fields.Many2one(comodel_name='olims.reference_sample', 
                     string='reference_sample_id', 
                     ondelete='cascade'),
-    fields.Integer(string="Expected Result"),
-    fields.Integer(string="Permitted Error"),
-    fields.Integer(string="Min"),
-    fields.Integer(string="Max"),
+    fields.Float(string="Expected Result"),
+    fields.Float(string="Permitted Error"),
+    fields.Float(string="Min"),
+    fields.Float(string="Max"),
+    fields.Many2one(string='Category',
+                    comodel_name='olims.analysis_category'),
           )
 
 class ReferenceValues(models.Model, BaseOLiMSModel): 
