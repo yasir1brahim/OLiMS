01e960e1cef482cd1504c2dc52260914a5152f58
yasir1brahim <yasir.ibrahim@gmail.com>
2016-01-04 09:24:09 +0500

    Re-factored the code to fetch data from model and render it to QWeb template.

--- report/olims_dataentrydaybook.py
+++ report/olims_dataentrydaybook.py
@@ -1,42 +1,55 @@
-from dependencies.dependency import ViewPageTemplateFile
-from lims import bikaMessageFactory as _
-from lims.browser import BrowserView
-from lims.browser.reports.selection_macros import SelectionMacrosView
-from dependencies.dependency import IViewView
-from dependencies.dependency import implements
+import time
+from openerp import api, models
+import datetime
 
 
-class Report(BrowserView):
-    implements(IViewView)
-    default_template = ViewPageTemplateFile("templates/productivity.pt")
-    template = ViewPageTemplateFile("templates/productivity_dataentrydaybook.pt")
+class ReportDataEntryDayBook(models.AbstractModel):
+    _name = 'report.OLiMS.report_dataentrydaybook'
 
-    def __init__(self, context, request, report=None):
-        super(Report, self).__init__(context, request)
-        self.report = report
-        self.selection_macros = SelectionMacrosView(self.context, self.request)
+    @api.multi
+    def render_html(self, data):
+        startdate = datetime.datetime.strptime(data['form'].get('date_from'), \
+            "%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d %H:%M:%S")
+        enddate = datetime.datetime.strptime(data['form'].get('date_to'), \
+            "%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d %H:%M:%S")
+        self.model = self.env.context.get('active_model')
+        docs = self.env[self.model].browse(self.env.context.get('active_id'))
+        AnalysesRequest = self.env['olims.analysis_request'].search([('create_date', '>=', startdate), \
+            ('create_date', '<=', enddate),
+            ])
+        datalines, footlines = self.with_context(data['form'].get('used_context'))._get_analyses(AnalysesRequest)
+        docargs = {
+            'doc_ids': self.ids,
+            'doc_model': self.model,
+            'data': data['form'],
+            'docs': docs,
+            'time': time,
+            'datalines': datalines,
+            'footlines' : footlines
+        }
+        return self.env['report'].render('OLiMS.report_dataentrydaybook', docargs)
 
-    def __call__(self):
+    def _get_analyses(self, AnalysesRequest):
 
-        parms = []
-        titles = []
+        # # parms = []
+        # # titles = []
 
-        # Apply filters
-        self.contentFilter = {'portal_type': 'AnalysisRequest'}
-        val = self.selection_macros.parse_daterange(self.request,
-                                                    'getDateCreated',
-                                                    _('Date Created'))
-        if val:
-            self.contentFilter[val['contentFilter'][0]] = val['contentFilter'][1]
-            parms.append(val['parms'])
-            titles.append(val['titles'])
+        # # # Apply filters
+        # # self.contentFilter = {'portal_type': 'AnalysisRequest'}
+        # # val = self.selection_macros.parse_daterange(self.request,
+        # #                                             'getDateCreated',
+        # #                                             _('Date Created'))
+        # # if val:
+        # #     self.contentFilter[val['contentFilter'][0]] = val['contentFilter'][1]
+        # #     parms.append(val['parms'])
+        # #     titles.append(val['titles'])
 
-        # Query the catalog and store results in a dictionary
-        ars = self.bika_catalog(self.contentFilter)
-        if not ars:
-            message = _("No Analysis Requests matched your query")
-            self.context.plone_utils.addPortalMessage(message, "error")
-            return self.default_template()
+        # # Query the catalog and store results in a dictionary
+        ars = AnalysesRequest
+        # if not ars:
+        #     message = _("No Analysis Requests matched your query")
+        #     self.context.plone_utils.addPortalMessage(message, "error")
+        #     return self.default_template()
 
         datalines = {}
         footlines = {}
@@ -46,31 +59,39 @@ class Report(BrowserView):
         totalanlcount = 0
         totalreceptionlag = 0
         totalpublicationlag = 0
+        datereceived = ''
+        datepublished = ''
 
         for ar in ars:
-            ar = ar.getObject()
-            datecreated = ar.created()
-            datereceived = ar.getDateReceived()
-            datepublished = ar.getDatePublished()
+            # ar = ar.getObject()
+            datecreated = datetime.datetime.strptime(ar.create_date, \
+            "%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d %H:%M:%S")
+            if ar.state == 'sample_received':
+                datereceived = datetime.datetime.strptime(ar.DateReceived, \
+            "%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d %H:%M:%S")
+            if ar.state == 'published':
+                datepublished = datetime.datetime.strptime(ar.DatePublished, \
+            "%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d %H:%M:%S")
+
             receptionlag = 0
             publicationlag = 0
-            anlcount = len(ar.getAnalyses())
+            anlcount = len(ar.FieldService.Service) or len(ar.FieldService.LabService)
 
             dataline = {
-                "AnalysisRequestID": ar.getRequestID(),
-                "DateCreated": self.ulocalized_time(datecreated),
-                "DateReceived": self.ulocalized_time(datereceived),
-                "DatePublished": self.ulocalized_time(datepublished),
+                "AnalysisRequestID": ar.RequestID,
+                "DateCreated": datecreated,
+                "DateReceived": datereceived,
+                "DatePublished": datepublished,
                 "ReceptionLag": receptionlag,
                 "PublicationLag": publicationlag,
                 "TotalLag": receptionlag + publicationlag,
-                "BatchID": ar.getBatch().getId() if ar.getBatch() else '',
-                "SampleID": ar.getSample().Title(),
-                "SampleType": ar.getSampleTypeTitle(),
+                "BatchID": ar.Batch.name if ar.Batch else '',
+                "SampleID": ar.Sample.SampleID,
+                "SampleType": ar.SampleType.SampleType,
                 "NumAnalyses": anlcount,
-                "ClientID": ar.aq_parent.id,
-                "Creator": ar.Creator(),
-                "Remarks": ar.getRemarks()
+                "ClientID": ar.Client.ClientID,
+                "Creator": ar.create_uid.name,
+                # "Remarks": #ar.Remarks
             }
 
             datalines[ar.getRequestID()] = dataline
@@ -111,61 +132,4 @@ class Report(BrowserView):
         }
 
         footlines['Total'] = footline
-
-        self.report_data = {'parameters': parms,
-                            'datalines': datalines,
-                            'footlines': footlines}
-
-        if self.request.get('output_format', '') == 'CSV':
-            import csv
-            import StringIO
-            import datetime
-
-            fieldnames = [
-                "AnalysisRequestID",
-                "DateCreated",
-                "DateReceived",
-                "DatePublished",
-                "ReceptionLag",
-                "PublicationLag",
-                "TotalLag",
-                "BatchID",
-                "SampleID",
-                "SampleType",
-                "NumAnalyses",
-                "ClientID",
-                "Creator",
-                "Remarks",
-            ]
-            output = StringIO.StringIO()
-            dw = csv.DictWriter(output, extrasaction='ignore',
-                                fieldnames=fieldnames)
-            dw.writerow(dict((fn, fn) for fn in fieldnames))
-            for ar_id, row in datalines.items():
-                dw.writerow({
-                    "AnalysisRequestID": row["AnalysisRequestID"],
-                    "DateCreated": row["DateCreated"],
-                    "DateReceived": row["DateReceived"],
-                    "DatePublished": row["DatePublished"],
-                    "ReceptionLag": row["ReceptionLag"],
-                    "PublicationLag": row["PublicationLag"],
-                    "TotalLag": row["TotalLag"],
-                    "BatchID": row["BatchID"],
-                    "SampleID": row["SampleID"],
-                    "SampleType": row["SampleType"],
-                    "NumAnalyses": row["NumAnalyses"],
-                    "ClientID": row["ClientID"],
-                    "Creator": row["Creator"],
-                    "Remarks": row["Remarks"],
-                })
-            report_data = output.getvalue()
-            output.close()
-            date = datetime.datetime.now().strftime("%Y%m%d%H%M")
-            setheader = self.request.RESPONSE.setHeader
-            setheader('Content-Type', 'text/csv')
-            setheader("Content-Disposition",
-                      "attachment;filename=\"dataentrydaybook_%s.csv\"" % date)
-            self.request.RESPONSE.write(report_data)
-        else:
-            return {'report_title': _('Data entry day book'),
-                    'report_data': self.template()}
+        return datalines, footlines
