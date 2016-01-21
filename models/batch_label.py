bca97aa2b70987259c88e05ec30c32405d85f42c
yasir1brahim <yasir.ibrahim@gmail.com>
2016-01-21 11:37:42 +0500

    Renamed field for tree view.

--- models/batch_label.py
+++ models/batch_label.py
@@ -2,11 +2,12 @@
 from openerp import fields, models
 from base_olims_model import BaseOLiMSModel
 
-schema = (fields.Char(string='name',required=True),
+schema = (fields.Char(string='Label',required=True),
           
           )
 
 class BatchLabel(models.Model, BaseOLiMSModel): 
     _name='olims.batch_label'
+    _rec_name = 'Label'
     
 BatchLabel.initialze(schema)
