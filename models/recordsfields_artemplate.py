d869b5b31eddcbc1303affb1555c2117a365b64a
yasir1brahim <yasir.ibrahim@gmail.com>
2016-01-21 12:58:55 +0500

    Define a model for 'Analyses' in Analysis Request inherit from records_field_artemplate.

--- models/recordsfields_artemplate.py
+++ models/recordsfields_artemplate.py
@@ -12,6 +12,15 @@ schema = (fields.Many2one(string='Services',
         fields.Many2one(string='Category',
                     comodel_name='olims.analysis_category'),
           )
+analysis_schema = (fields.Many2one(string='Priority',
+                                   comodel_name='olims.ar_priority'),
+                   fields.Many2one(string='Partition',
+                                   comodel_name='olims.ar_partition'),
+                   fields.Char(string="Error"),
+                   fields.Char(string="Min"),
+                   fields.Char(string="Max"),
+                   fields.Many2one(string='analysis_request_id', comodel_name ='olims.analysis_request'),
+                   )
 
 class RecodrdsFieldARTemplate(models.Model, BaseOLiMSModel): 
     _name='olims.records_field_artemplates'
@@ -27,5 +36,9 @@ class RecodrdsFieldARTemplate(models.Model, BaseOLiMSModel):
         # set auto-changing field
         if self.Services:
             self.Hidden = self.Services.Hidden
-
+class ARAnalysis(models.Model, BaseOLiMSModel):
+    _inherit = 'olims.records_field_artemplates'
+    _name = 'olims.ar_analysis'
+    
 RecodrdsFieldARTemplate.initialze(schema)
+ARAnalysis.initialze(analysis_schema)
