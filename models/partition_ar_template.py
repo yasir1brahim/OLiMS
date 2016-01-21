f69b905cb7b102fbd86059378ff5209b1b8aef49
yasir1brahim <yasir.ibrahim@gmail.com>
2016-01-21 12:48:57 +0500

    Define a model for 'Analysis Request Sample Partition' inherit from partition_ar_template.

--- models/partition_ar_template.py
+++ models/partition_ar_template.py
@@ -7,9 +7,16 @@ schema = (
           fields.Many2one(string='Preservation', comodel_name ='olims.preservation'),
           fields.Char('Partition'),
           )
-
+ar_partition_schema = (fields.Char('state'),
+                       fields.Many2one(string='analysis_request_id', comodel_name ='olims.analysis_request'),
+#                        fields.Char('partition_id')
+                       )
 class PartitionARTemplate(models.Model, BaseOLiMSModel): 
     _name='olims.partition_ar_template'
     _rec_name = 'Partition'
-    
+
+class ARPartition(models.Model, BaseOLiMSModel):
+    _inherit = 'olims.partition_ar_template'
+    _name='olims.ar_partition'
 PartitionARTemplate.initialze(schema)
+ARPartition.initialze(ar_partition_schema)
