0609a49fd65b9ce1d2ac4749a06cf4e45c61baf1
yasir1brahim <yasir.ibrahim@gmail.com>
2016-04-21 18:22:41 +0500

    Resolved the mereges issues.

--- models.py
+++ models.py
@@ -1,35 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-from HTMLParser import HTMLParser
 from StringIO import StringIO
 import base64
 import csv
 import datetime
-import logging
-from openerp import http
-from openerp import models, fields, api
 from openerp import models, fields, api, netsvc
 from openerp.tools.translate import _
-import shutil
-import tempfile
-import StringIO
-import csv
-from openerp import http
-from openerp.http import request
-
-import tkFileDialog as filedialog
-from wizard.export_experiment import generate_csv 
+import StringIO 
 import openerp
 from openerp import SUPERUSER_ID
 import json
-import gzip
 import zipfile
-from zipfile import ZipFile
-import zlib
-
-
-#from openerp.http import request
 
 
 AVAILABLE_PRIORITIES = [
@@ -41,15 +23,10 @@ AVAILABLE_PRIORITIES = [
     ('5', 'Very High'),
     ]
 
-
-class Labpal_Mail(models.Model):
 class InMemoryZip(object):
     def __init__(self):
         # Create the in-memory file-like object
         self.in_memory_zip = StringIO.StringIO()
-
-    _name = 'labpal.mail'
-    email_From = fields.Char('Description')
 
     def append(self, filename_in_zip, file_contents,filename_in_zip_csv, file_contents_csv):
         '''Appends a file with name filename_in_zip and contents of 
@@ -170,11 +147,6 @@ class Experiment(models.Model):
         }
 
     @api.multi
-    def get_csv_db(self):
-
-        pass
-
-    @api.multi
     def experiment_pdf_export(self):
         
         #fetch the qweb-pdf file
@@ -194,118 +166,11 @@ class Experiment(models.Model):
      }
 
     @api.multi
-    def file_compression_old(self):
-
-        file_csv_location = tempfile.gettempdir()
-        source = os.path.join(file_csv_location, 'TESTING/')
-
-        # shutil.make_archive('destination','zip','source')
-
-        outputquery = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(query)
-            
-        csv_file = StringIO()
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["1","a","b","c"])
-
-        conn.close()
-
-    @api.multi
-    def get_csv_db(self):
-        filename = expanduser("~")
-        filename = os.path.join('labpaldb.csv')
-
-        query_desc = "SELECT description FROM labpal_database where id="+ str(self.id) + ""
-        join_q = "SELECT tag_id FROM database_tag_rel where database_id="+ str(self.id) + ""
-
-        conn = psycopg2.connect("dbname = 'labpal' user = 'dev' host = 'localhost' password = 'labpal9'")
-        cur = conn.cursor()
-
-        cur.execute(join_q)
-        tagid = cur.fetchone()
-        tagid = str(tagid[0])
-
-        query = "select t1.name,t1.exp_date,t2.name,t1.description from labpal_database t1,labpal_tag t2 where t1.id="+str(self.id)+" and t2.id ="+ tagid+""
-
-
-
-        cur.execute(query_desc)
-
-        result = cur.fetchone()
-        result = "'"+result[0]+"'"
-        s = MLStripper()
-        s.feed(result)
-        result = s.get_data()
-        result = result.replace('/n','')
-        
-        #result = str(result)
-
-        update_query = "UPDATE labpal_database SET description="+result+" WHERE id="+str(self.id)+""
-       
-
-    @api.multi
-    def file_compression(self):
-
-        data ={
-           "model": 'labpal.experiment',
-           "fields":[
-                       {"name":"id","label":"External ID"},
-                       # {"name":"attachment_ids/id","label":"Attachments"},
-                       # {"name":"create_uid/id","label":"Created by"},
-                       # {"name":"create_date","label":"Created on"},
-                       # {"name":"database_ids/id","label":"Database"},
-                       # {"name":"exp_date","label":"Date"},
-                       # {"name":"description","label":"Description"},
-                       # {"name":"write_uid/id","label":"Last Updated by"},
-                       # {"name":"write_date","label":"Last Updated on"},
-                       # {"name":"exp_status/id","label":"Status"},
-                       # {"name":"tag_ids/id","label":"Tags"},
-                       # {"name":"template_id/id","label":"Template"},
-                       {"name":"exp_title","label":"Title"},
-                       # {"name":"status_visibility","label":"Visibility"}
-                       ],
-            "ids":self.id,
-            "domain":[],
-            "context":{"lang":"en_US",
-                       "tz":False,
-                       "uid":1,
-                       "params":{
-                                 "action":79,
-                                 "page":0,
-#                                  "limit":80,
-                                 "view_type":"list",
-                                 "model": 'labpal.experiment',
-                                 "_push_me":False
-                                 }
-                       },
-            "import_compat":True
-            }
-
-        #getting experiment csv 
+    def file_compression(self):#getting experiment csv 
         csv_file = StringIO.StringIO()
-        # data = data['fields'][0:]
 
-        content = json.dumps(data)
-        with open(filename, 'w+') as f:
-                cur.copy_expert(outputquery , f)
-        conn.close()
-        webbrowser.open(filename)
-        return True
         labpal = self.env['labpal.experiment']
 
-    @api.multi
-    def get_pdf(self):
-        file = self.env['report'].get_action(self, 'labpal.pdf_template')
-        return self.env['report'].get_action(self, 'labpal.pdf_template')
-
-    @api.multi    
-    def get_pdf_db(self):
-        return self.env['report'].get_action(self, 'labpal.pdf_db_template')    
-
-    @api.model
-    def _get_default_status(self):
-        res = self.env['labpal.status'].search([('default_status','=',True)])
-        if res:
-            return res[0] 
         experiment = labpal.browse(self.id)
         data = {}
         tag = []
@@ -313,15 +178,6 @@ class Experiment(models.Model):
         if experiment.exp_title:
             title = experiment.exp_title
         else:
-            False
-        writer = csv.writer(csv_file)
-        writer.writerows(content)
-
-        #compressing the StringIO csv object
-
-        # zip_archive = zipfile.ZipFile(csv_file,'w')
-        # zip_archive.writestr('labpal_test.csv',csv_file.getvalue())
-        # zip_archive.close()
             title = 'Untitled'
         data["Experiment Title"] = title
         data["Date"] = experiment.exp_date
@@ -342,42 +198,14 @@ class Experiment(models.Model):
             service = netsvc.LocalService(report_service)
             result, format = openerp.report.render_report(self.env.cr, self.env.uid, [self.id], ir_actions_report.report_name, {'model': self._name}, context=self.env.context)
 
-        file = self.env['report'].get_action(self, 'labpal.pdf_template')
-        # zip_archive_pdf = zipfile.ZipFile(file,'w')
-        # zip_archive_pdf.write()
-
-        zip = zipfile.ZipFile('THE_FOLDER_.zip','a')
-        zip.writestr('odoo.csv',csv_file.getvalue())
-        # zip.write('/home/developer06/Desktop/Test_LP.pdf')
-
-        ir_actions_report = self.pool.get('ir.actions.report.xml')
         imz = InMemoryZip()
         imz.append(str(title)+'.pdf',result,str(title)+'.csv',csv_file.getvalue())
 
-        report_obj = self.env['report']
         compressed_file = imz.read()
 
-        report_in = report_obj._get_report_from_name('labpal.pdf_template')
-
-        datas = {}
-        matching_reports = ir_actions_report.search(
-            self.env.cr,self.env.uid, [('name', '=', 'Download as a pdf file')])
-        matching_reports = matching_reports[1]
-    
-        if matching_reports:
-            report = ir_actions_report.browse(self.env.cr, self.env.uid, matching_reports)
-            report_service = 'report.' + report.report_name
-            # service = netsvc.LocalService(report_service)
-            result, format = openerp.report.render_report(self.env.cr, self.env.uid,[1],'labpal.pdf_template',datas,{})
-            result = base64.b64encode(result)
-            file_name = "odooo.pdf"
-
-        zip.write(file)
         attachment_obj = self.pool.get('ir.attachment')
 
         filename = str(datetime.date.today())+'-'+str(title)+'-'+'labpal.zip'
-
-        zip.close()
 
         new_attachemet_obj = attachment_obj.create(self.env.cr, self.env.uid,
                                                       {
@@ -389,29 +217,18 @@ class Experiment(models.Model):
 
         return {
              'type' : 'ir.actions.act_url',
-             'url': '/web/export/csv?data=' + json.dumps(data) + '&token=' + str(None),
-             'target': 'self'
-        }
-
-        
-
-        
-
-
-    @api.multi
-    def get_zip():
-        file = self.env['report'].get_action(self, 'labpal.pdf_template')
              'url': '/web/content/'+str(new_attachemet_obj)+'?download=true',
              }
 
-    
     @api.multi
-    def test_form(self):
+    def experiment_mail_compose(self):
         '''
         This function opens a window to compose an email, with the edi sale template message loaded by default
         '''
+
         self.ensure_one()
         ir_model_data = self.env['ir.model.data']
+
         try:
             template_id = ir_model_data.get_object_reference('labpal', 'labpal_send_report_mail_template')[1]
         except ValueError:
@@ -420,6 +237,7 @@ class Experiment(models.Model):
             compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
         except ValueError:
             compose_form_id = False
+
         ctx = dict()
         ctx.update({
             'default_model': 'labpal.experiment',
@@ -429,6 +247,7 @@ class Experiment(models.Model):
             'default_composition_mode': 'comment',
             'mark_so_as_sent': True
         })
+      
         return {
             'type': 'ir.actions.act_window',
             'view_type': 'form',
@@ -439,7 +258,6 @@ class Experiment(models.Model):
             'target': 'new',
             'context': ctx,
         }
-
 
 class Status(models.Model):
 
