03d5ae060f5b2690d0fc4c4df0cfb53cc7981963
yasir1brahim <yasir.ibrahim@gmail.com>
2017-01-18 16:23:03 +0500

    Changed field type to allow  multiple selection of Analysis Services and allowed to search with ">" or "<? operator.

--- models/search_ar.py
+++ models/search_ar.py
@@ -1,23 +1,17 @@
 # worksheet search on analysis basis.
 
-from openerp import models, fields, api, netsvc
+from openerp import models, fields, api
+from openerp.tools.translate import _
 
 class SerachWroksheet(models.TransientModel):
     _name = "olims.search_wroksheet"
-
-    @api.multi
-    def _selection_services(self):
-        list_of_name = []
-        items_types_object = self.env['olims.analysis_service'].search([])
-        for name in items_types_object:
-            list_of_name.append((name.id,name.Service))
-        return list_of_name
+    _rec_name = "selection_type"
 
     @api.multi
     def _selection_type(self):
         list_of_name = [('analysis','Analysis Request'),('worksheet','Worksheet')]
         return list_of_name
-    analysis_field = fields.Selection(_selection_services, 'Search in', default='', required=True)
+    analysis_service_ids = fields.Many2many(comodel_name="olims.analysis_service", string='Search in', required=True)
     selection_type = fields.Selection(_selection_type, 'Search in', default='analysis', required=True)
     Result_field = fields.Char('Result')
 
@@ -25,10 +19,26 @@ class SerachWroksheet(models.TransientModel):
     def search_ar_data(self):
         data = self.read()[0]
         ids_list = []
-        analysis_field = data['analysis_field']
-        res_query = ('result_string','=',Result) if data['Result_field'] else ('id','>',0)
+        analysis_field = data['analysis_service_ids']
+        if data['Result_field']:
+        	if data['Result_field'].find('>') >= 0:
+        		result_val = float(data['Result_field'].split(">")[1])
+        		res_query_ws = ('result','>', result_val)
+        		res_query_ar = ('Result','>', result_val)
+        	elif data['Result_field'].find('<') >= 0:
+        		result_val = float(data['Result_field'].split("<")[1])
+        		res_query_ws = ('result','<', result_val)
+        		res_query_ar = ('Result','<', result_val)
+        	else:
+        		result_val = float(data['Result_field'])
+        		res_query_ws = ('result','=', result_val)
+        		res_query_ar = ('Result','=', result_val)
+        else:
+        	res_query_ws = ('id','>',0)
+        	res_query_ar = ('id','>',0)
         if data['selection_type'] =='worksheet':
-        	ids_list_ws_manage_res = self.env['olims.ws_manage_results'].search([('analysis', '=', int(analysis_field)),res_query])
+        	ids_list_ws_manage_res = self.env['olims.ws_manage_results'].search([
+        		('analysis', 'in', analysis_field),res_query_ws])
 	        ws_res_ids = []
 	        for item in ids_list_ws_manage_res:
 	        	ws_res_ids.append(item.id)
@@ -38,8 +48,10 @@ class SerachWroksheet(models.TransientModel):
 	        	ws_id_list.append(ws_id.id)
 	       	if len(ws_id_list) > 0:
 	            return{
+	            	'name':_('WorkSheets'),
 	                'views': [[False, 'tree'],[False, "form"]],
 	                'res_model': 'olims.worksheet',
+	                'target': 'current',
 	                'type': 'ir.actions.act_window',
 	                'domain': [['id', 'in', ws_id_list]],
 	            }
@@ -51,9 +63,8 @@ class SerachWroksheet(models.TransientModel):
 	                'target' : 'inline'
 	            }
         else:
-	    	res_query = ('result_string','=',Result) if data['Result_field'] else ('id','>',0)
-	    	ids_list_ws_ar_res = self.env["olims.manage_analyses"].search(["|",("Service","=",int(analysis_field))
-                    ,("LabService","=",int(analysis_field)), res_query])
+	    	ids_list_ws_ar_res = self.env["olims.manage_analyses"].search(["|",("Service","in",analysis_field)
+                    ,("LabService","in",analysis_field), res_query_ar])
 
 	    	manage_analy_list = []
 	    	for ar_id in ids_list_ws_ar_res:
@@ -68,8 +79,10 @@ class SerachWroksheet(models.TransientModel):
 
 	        if len(ar_ids) > 0:
 	            return{
+	            	'name':_('Analysis Requests'),
 	                'views': [[False, 'tree'],[False, "form"]],
 	                'res_model': 'olims.analysis_request',
+	                'target': 'current',
 	                'type': 'ir.actions.act_window',
 	                'domain': [['id', 'in', ar_ids]],
 	            }
