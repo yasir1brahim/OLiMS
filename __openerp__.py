82fd510472a0ace284cbfb8311cdf14548bd2582
yasir1brahim <yasir.ibrahim@gmail.com>
2017-05-05 11:25:37 +0500

    Changed label of 'Email' to 'Username' for login page

--- __openerp__.py
+++ __openerp__.py
@@ -80,6 +80,7 @@
         'views/analysis_request_to_be_invoiced_report.xml',
         'views/ar_invoice_mail_template.xml',
         'views/search_ar.xml',
+        'views/weblogin.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
