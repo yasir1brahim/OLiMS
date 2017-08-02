468505c4a5f5c91cca5d7e899d4ebe806f436597
yasir1brahim <yasir.ibrahim@gmail.com>
2017-08-02 11:00:34 +0500

    Fixed type issue when record is not exist.

--- models/worksheet.py
+++ models/worksheet.py
@@ -133,15 +133,18 @@ class Worksheet(models.Model, BaseOLiMSModel):
             counter = 1
             c_date = datetime.datetime.strptime(items.create_date, "%Y-%m-%d %H:%M:%S").strftime("%y,%m,%d")
             year, month, day = c_date.split(',')
-            if items.Template:
+            if items.Template and isinstance(items.Template.Title,str):
                 temp_name =  items.Template.Title + " " + items.category_name
             else:
                 temp_name = items.category_name
             if temp_name in name_counter.keys() and name_counter[temp_name][1] == c_date:
                 counter = name_counter.get(temp_name)[0] + 1
+            if not temp_name:
+                temp_name = ''
             worksheetid = temp_name + " " + month + day + year + "-" +str(counter)
             name_counter[temp_name] = [counter, c_date]
             items.Worksheet = worksheetid
+
 
     @api.depends("Template")
     def _Computecategory_name(self):
