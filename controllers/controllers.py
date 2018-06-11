e094aff7debd8f079f8a10b8e1fba801407ba011
yasir1brahim <yasir.ibrahim@gmail.com>
2018-06-11 15:22:54 +0500

    Completed Task : When sending emails, the system created the COA filename by combining the Lot ID with the sample Name. Configure (Or discuse) system so all COAs are named in this manner when generated.

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -11,34 +11,53 @@ from openerp.addons.web.controllers.main import serialize_exception,content_disp
 import base64
 import StringIO
 
-class Binary(http.Controller):
- @http.route('/web/binary/download_document', type='http', auth="public")
- @serialize_exception
- def download_document(self,model,field,id,filename=None, **kw):
-     """ Download link for files stored as binary fields.
-     :param str model: name of the model to fetch the binary from
-     :param str field: binary field
-     :param str id: id of the record from which to fetch the binary
-     :param str filename: field holding the file's name, if any
-     :returns: :class:`werkzeug.wrappers.Response`
-     """
-     Model = request.registry['labpal.experiment']
-     cr, uid, context = request.cr, request.uid, request.context
-     fields = [field]
-     res = Model.read(cr, uid, [int(id)], fields, context)[0]
-     csv_file = StringIO()
-     csv_writer = csv.writer(csv_file)
-     csv_writer.writerow(["1","a","b","c"])
-     filecontent = base64.b64decode(csv_file)
+import pdb
+from openerp.addons.web.http import Controller, route
+from openerp.addons.report.controllers.main import ReportController
+from openerp.addons.web.controllers.main import _serialize_exception, content_disposition
+from openerp.osv import osv
+from openerp import http
+import simplejson
+import time
+import logging
+from werkzeug import exceptions, url_decode
+from werkzeug.test import Client
+from werkzeug.wrappers import BaseResponse
+from werkzeug.datastructures import Headers
+import openerp.tools as tools
+import openerp.addons.decimal_precision as dp
+from openerp.tools import html_escape
+import re
 
-     if not filecontent:
-         return request.not_found()
-     else:
-         if not filename:
-             filename = '%s_%s' % (model.replace('.', '_'), id)
-             return request.make_response(filecontent,
-                            [('Content-Type', 'application/octet-stream'),
-                             ('Content-Disposition', content_disposition(filename))])     
+
+class Binary(http.Controller):
+    @http.route('/web/binary/download_document', type='http', auth="public")
+    @serialize_exception
+    def download_document(self, model, field, id, filename=None, **kw):
+        """ Download link for files stored as binary fields.
+        :param str model: name of the model to fetch the binary from
+        :param str field: binary field
+        :param str id: id of the record from which to fetch the binary
+        :param str filename: field holding the file's name, if any
+        :returns: :class:`werkzeug.wrappers.Response`
+        """
+        Model = request.registry['labpal.experiment']
+        cr, uid, context = request.cr, request.uid, request.context
+        fields = [field]
+        res = Model.read(cr, uid, [int(id)], fields, context)[0]
+        csv_file = StringIO()
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["1", "a", "b", "c"])
+        filecontent = base64.b64decode(csv_file)
+
+        if not filecontent:
+            return request.not_found()
+        else:
+            if not filename:
+                filename = '%s_%s' % (model.replace('.', '_'), id)
+                return request.make_response(filecontent,
+                                             [('Content-Type', 'application/octet-stream'),
+                                              ('Content-Disposition', content_disposition(filename))])
 
 
 class Labpal(http.Controller):
@@ -63,3 +82,77 @@ class Labpal(http.Controller):
     def index(self, **kw):
         return http.request.render( "olims.listing", {})
 
+
+class NewReportController(ReportController):
+    @route(['/report/download'], type='http', auth="user")
+    def report_download(self, data, token):
+        """This function is used by 'qwebactionmanager.js' in order to trigger the download of
+        a pdf/controller report.
+
+        :param data: a javascript array JSON.stringified containg report internal url ([0]) and
+        type [1]
+        :returns: Response with a filetoken cookie and an attachment header
+        """
+        requestcontent = simplejson.loads(data)
+        url, type = requestcontent[0], requestcontent[1]
+        try:
+
+            if type == 'qweb-pdf':
+                reportname = url.split('/report/pdf/')[1].split('?')[0]
+                cr, uid, context = request.cr, request.uid, request.context
+                report_file = reportname.split('/')[0]
+                report = request.registry['report']._get_report_from_name(cr, uid, report_file)
+                filename = "%s.%s" % (report.name, "pdf")
+                docids = None
+
+                if '/' in reportname:
+                    reportname, docids = reportname.split('/')
+                    print 'doc ids',docids
+                if docids:
+                    # Generic report:
+                    response = self.report_routes(reportname, docids=docids, converter='pdf')
+                    # switch reportname with the evaluated attachment attribute of the action if available
+                    docids = [int(i) for i in docids.split(',')]
+                    if reportname == 'olims.report_certificate_of_analysis' and len(docids) == 1:
+                        report_obj = request.registry['report']
+                        obj = report_obj.pool[report.model].browse(cr, uid, docids[0])
+
+                        if obj.ClientReference and not obj.LotID:
+                            reportname = obj.ClientReference
+
+                        elif obj.LotID and not obj.ClientReference:
+                            reportname = obj.LotID
+
+                        elif obj.LotID and  obj.ClientReference:
+                            reportname = obj.LotID +'-'+ obj.ClientReference
+
+                        else:
+                            reportname = "COA"
+
+                        filename = "%s.%s" % (reportname, "pdf")
+
+                else:
+                    # Particular report:
+                    data = url_decode(url.split('?')[1]).items()  # decoding the args represented in JSON
+                    response = self.report_routes(reportname, converter='pdf', **dict(data))
+                response.headers.add('Content-Disposition', content_disposition(filename))
+                response.set_cookie('fileToken', token)
+                return response
+
+            elif type == 'controller':
+                reqheaders = Headers(request.httprequest.headers)
+                response = Client(request.httprequest.app, BaseResponse).get(url, headers=reqheaders, follow_redirects=True)
+                response.set_cookie('fileToken', token)
+                return response
+
+            else:
+                return
+
+        except Exception, e:
+            se = _serialize_exception(e)
+            error = {
+                'code': 200,
+                'message': "Odoo Server Error",
+                'data': se
+            }
+            return request.make_response(html_escape(simplejson.dumps(error)))
