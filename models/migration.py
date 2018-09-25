b045fc8f6f0829f9b19b56ad78f3bb2490cfbf71
yasir1brahim <yasir.ibrahim@gmail.com>
2018-09-25 14:54:32 +0459

    Added Migration script for AnalysisProfile field for new deployment.

--- models/migration.py
+++ models/migration.py
@@ -47,3 +47,27 @@ class WorksheetMigration(models.Model):
 			profile_object = self.env["olims.analysis_profile"].search([('id', '=', profile.id )])
 			profile_object.write({"name":profile.Profile})
 		return
+
+	@api.model
+	def migration_analysisprofile_filed_ar(self):
+		analysis_requests = self.env["olims.analysis_request"].search([('id', '>', 0)])
+		for request in analysis_requests:
+			query = "select " + '"AnalysisProfile",' + '"AnalysisProfile1",' + '"AnalysisProfile2",' + '"AnalysisProfile3"' + "from olims_analysis_request where id = " + "'" + str(
+				request.id) + "'"
+			self.env.cr.execute(query)
+			profiles_list = self.env.cr.fetchall()
+			AnalysisProfile, AnalysisProfile1, AnalysisProfile2, AnalysisProfile3 = None, None, None, None
+			print '---profiles list', profiles_list[0]
+			if profiles_list[0][0]:
+				AnalysisProfile = [(6, 0, [profiles_list[0][0]])]
+			if profiles_list[0][1]:
+				AnalysisProfile1 = [(6, 0, [profiles_list[0][1]])]
+			if profiles_list[0][2]:
+				AnalysisProfile2 = [(6, 0, [profiles_list[0][2]])]
+			if profiles_list[0][3]:
+				AnalysisProfile3 = [(6, 0, [profiles_list[0][3]])]
+			request.write({'AnalysisProfile': AnalysisProfile, 'AnalysisProfile1': AnalysisProfile1, 'AnalysisProfile2': \
+				AnalysisProfile2, 'AnalysisProfile3': AnalysisProfile3})
+
+		return
+
