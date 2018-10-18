6b98bfe5539d1ad55abba7988f48441c4642a090
yasir1brahim <yasir.ibrahim@gmail.com>
2018-10-18 15:28:37 +0500

    Assigned Serial number to Sample under Manage Result tab under worksheet on the basis of AR number

--- models/worksheet.py
+++ models/worksheet.py
@@ -1260,7 +1260,7 @@ class WorkSheetManageResults(models.Model):
     @api.depends("result_string")
     def calc_serial_ws(self):
         count = 0
-        for record in self:
+        for record in sorted(self, key=lambda x: (x['request_analysis_id'].id)):
             count = count + 1
             record.serial = count
 
